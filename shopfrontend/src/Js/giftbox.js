82954a873e3397f333f44cd84cf02cd24e4df895
Hashinie-Madhushika <madhushikahashinie45@gmail.com>
2025-01-21 22:55:58 +0530

    Desktop App changes

--- shopfrontend/src/Js/giftbox.js
+++ shopfrontend/src/Js/giftbox.js
@@ -2,12 +2,23 @@ import React from 'react';
 import Footer from './Footer';
 import Header from "./Header";
 import "../Css/giftbox.css";
+import Im1 from "../Images/Pack1.jpg";
+import Im2 from "../Images/Pack1.jpg";
 
 const GiftBox = () =>{
     return(
         <div>
             <Header/>
             
+            <div className="packages-container">
+                    <img src={Im2} alt="main"/>
+                    <div className="image-container">
+                    <img src={Im1} alt="Packages" />
+                   
+                </div>
+                
+            </div>
+
             <Footer/>
         </div>
     )
